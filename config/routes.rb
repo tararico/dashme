0db6dcb80e35c2fde177e086bc62548868d6f8bf
tararico <emipu.uq@gmail.com>
2019-07-02 16:38:04 +0900

    routesを調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :buttons
+  root to: redirect('/buttons')
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
