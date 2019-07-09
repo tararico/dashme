87338efb5774553804a98695f8efc8ec9e024a3d
tararico <emipu.uq@gmail.com>
2019-07-09 19:10:14 +0900

    add active_storage

--- app/controllers/buttons_controller.rb
+++ app/controllers/buttons_controller.rb
@@ -69,6 +69,6 @@ class ButtonsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def button_params
-      params.require(:button).permit(:name)
+      params.require(:button).permit(:name, :avatar)
     end
 end
--- app/models/button.rb
+++ app/models/button.rb
@@ -1,3 +1,9 @@
 class Button < ApplicationRecord
   has_many :items
+  has_one_attached :avatar
+
+  # def thumbnail
+  #   return self.variant(resize: '100x100').processed
+  # end
+
 end
