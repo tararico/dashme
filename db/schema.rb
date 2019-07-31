73136f0f0352d48d35cb2484b1f847492b854f93
tararico <emipu.uq@gmail.com>
2019-07-31 10:38:31 +0900

    usersテーブルのカラムを変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_22_051205) do
+ActiveRecord::Schema.define(version: 2019_07_29_084717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,15 @@ ActiveRecord::Schema.define(version: 2019_07_22_051205) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["button_id"], name: "index_items_on_button_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
