d651d9f9bbaf967557fa740fa1542b1cf8ef2cd6
tararico <emipu.uq@gmail.com>
2019-08-07 13:49:48 +0900

    ログイン機能を作る

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get   '/signout',   to: 'users#new',  as: :signout
   post   '/signout',   to: 'users#create'
+  get   '/login',   to: 'user_sessions#new',  as: :login
+  post   '/login',   to: 'user_sessions#create'
+  delete '/logout',  to: 'user_sessions#destroy', as: :logout
   resources :users, only: [:new, :create]
   resources :buttons
   root to: 'buttons#index'
