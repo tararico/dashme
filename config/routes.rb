000e02423dea289708a9604436e21468e45056f5
tararico <emipu.uq@gmail.com>
2019-09-10 16:14:57 +0900

    ファミリー確認ページを作った

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'families/show'
   get   '/signout',   to: 'users#new',  as: :signout
   post   '/signout',   to: 'users#create'
   get   '/login',   to: 'user_sessions#new',  as: :login
   post   '/login',   to: 'user_sessions#create'
   delete '/logout',  to: 'user_sessions#destroy', as: :logout
   resources :users, except: :index
+  resources :families, only: [:show]
+  resources :invitations, only: [:create, :edit, :update, :show]
   resources :buttons
   root to: 'buttons#index'
   resources :items
