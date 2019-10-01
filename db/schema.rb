c61adefc0a8d76350ae053ec2fe59cb0c06fb955
tararico <emipu.uq@gmail.com>
2019-10-01 13:12:13 +0900

    slackworkspaceテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_18_011541) do
+ActiveRecord::Schema.define(version: 2019_09_24_080502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,14 @@ ActiveRecord::Schema.define(version: 2019_09_18_011541) do
     t.index ["button_id"], name: "index_items_on_button_id"
   end
 
+  create_table "slack_workspaces", force: :cascade do |t|
+    t.string "webhook_url"
+    t.bigint "family_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["family_id"], name: "index_slack_workspaces_on_family_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "crypted_password"
@@ -84,5 +92,6 @@ ActiveRecord::Schema.define(version: 2019_09_18_011541) do
   add_foreign_key "buttons", "families"
   add_foreign_key "invitations", "families"
   add_foreign_key "items", "buttons"
+  add_foreign_key "slack_workspaces", "families"
   add_foreign_key "users", "families"
 end
