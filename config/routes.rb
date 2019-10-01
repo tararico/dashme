a484cf2bee430d8e9c3dcaeb762486589f0472a9
tararico <emipu.uq@gmail.com>
2019-10-01 13:16:24 +0900

    webhookurlを登録するページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :families, only: [:show]
   resources :invitations, only: [:create, :edit, :update, :show]
   resources :buttons
+  resources :slack_workspaces
   root to: 'buttons#index'
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
