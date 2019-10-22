a3be5b19041be1deab8662c284d629fd5b3bd05e
tararico <emipu.uq@gmail.com>
2019-10-22 16:43:32 +0900

    create_digestのテストを書いた

--- test/models/invitation_test.rb
+++ test/models/invitation_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class InvitationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test " create_digest" do
+    invitation = Invitation.create
+    assert(invitation.invitation_digest == nil)
+    invitation.create_digest
+    assert_not(invitation.invitation_digest == nil)
+  end
+
+  test "authenticated?" do
+    # invitation_digest.nilだったら、falseを返すこと
+    #invitation_digestとinvitation_tokenを暗号化たものが一致していたらtrueを返すこと
+    #一致していなかったらfalseを返すこと
+
+  end
+
+  test "expired?" do
+    #24時間以上立っていたらfalseを返す(24時間の以降のレコードを作る)
+    #以内だったらtrueを返す
+  end
+
 end
