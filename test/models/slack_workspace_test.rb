f6189b9607f867415ff9fd40c0a9c78ea3263d3b
tararico <emipu.uq@gmail.com>
2019-10-26 15:52:43 +0900

    notifymメソッドのテストを書いた

--- test/models/slack_workspace_test.rb
+++ test/models/slack_workspace_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
+require "minitest/mock"
 
 class SlackWorkspaceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "notify" do
+    text = "hoge"
+    link = "https://hoge.com"
+    workspace = SlackWorkspace.new
+
+    mock_log = ""
+    stub_info = Proc.new { |i| mock_log << i }
+    Rails.logger.stub(:info, stub_info) do
+      workspace.notify(text, link)
+      assert_match "Notify\ntext:#{text}\nlink:#{link}", mock_log
+    end
+  end
 end
