0456e1bb9dc57e8410e630d07df051a79a9dc7ec
tararico <emipu.uq@gmail.com>
2019-10-27 18:00:17 +0900

    controllerテストは不要

--- test/controllers/families_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class FamiliesControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get families_show_url
-    assert_response :success
-  end
-
-end
